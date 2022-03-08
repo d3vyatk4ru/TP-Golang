745c5484847700a76bffbcb5a73685c5f6fe15e2
d3vyatk4ru <danya.devyatkin@mail.ru>
2022-03-08 16:49:21 +0300

    init done

--- game/main.go
+++ game/main.go
@@ -1,9 +1,76 @@
 package main
 
-/*
-	код писать в этом файле
-	наверняка у вас будут какие-то структуры с методами, глобальные перменные ( тут можно ), функции
-*/
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+var player Player
+var room []Room
+
+func getData(fileName string) []string {
+
+	f, err := os.Open(fileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(f)
+
+	var data []string
+
+	for scanner.Scan() {
+
+		data = append(data, scanner.Text())
+	}
+
+	return data
+}
+
+func getString(fileName string, num_str int) string {
+
+	f, err := os.Open(fileName)
+
+	if err != nil {
+		fmt.Println("ooops!!!!")
+	}
+
+	scanner := bufio.NewScanner(f)
+
+	var data string
+
+	for i := 0; i <= num_str; i++ {
+
+		scanner.Scan()
+
+		if i == num_str {
+			data = scanner.Text()
+		}
+	}
+
+	return data
+}
+
+type Room struct {
+	Title       string
+	Id          int
+	Description string
+	exit        []string
+	Invetory    map[string]bool
+}
+
+type Player struct {
+	CurrentRoom string
+	Invetory    map[string]bool
+}
+
+func (player *Player) do(userAction string) {
+
+}
 
 func main() {
 	/*
@@ -11,13 +78,43 @@ func main() {
 		но тогда у вас не будет работать через go run main.go
 		очень круто будет сделать построчный ввод команд тут, хотя это и не требуется по заданию
 	*/
+
+	// initGame()
+
+	handleCommand("применить ключи шкаф")
+
 }
 
+/*
+	эта функция инициализирует игровой мир - все команты
+	если что-то было - оно корректно перезатирается
+*/
+
 func initGame() {
-	/*
-		эта функция инициализирует игровой мир - все команты
-		если что-то было - оно корректно перезатирается
-	*/
+
+	rooms := getData("rooms.txt")
+	player.CurrentRoom = rooms[0]
+
+	room = make([]Room, 2*len(rooms), 2*len(rooms))
+
+	for i, val := range rooms {
+		room[i].Title = val
+		room[i].Id = i
+		room[i].Description = getData("initDescription.txt")[i]
+		room[i].exit = strings.Split(getString("exit.txt", i), " ")
+		room[i].Invetory = make(map[string]bool)
+
+		if str := getString("inventory.txt", i); str != "" {
+			for _, val := range strings.Split(str, " ") {
+				room[i].Invetory[val] = true
+			}
+		}
+	}
+
+	player.Invetory = make(map[string]bool)
+	for _, val := range getData("things.txt") {
+		player.Invetory[val] = false
+	}
 }
 
 func handleCommand(command string) string {
@@ -25,5 +122,8 @@ func handleCommand(command string) string {
 		данная функция принимает команду от "пользователя"
 		и наверняка вызывает какой-то другой метод или функцию у "мира" - списка комнат
 	*/
+
+	splt := strings.Split(command, " ")
+
 	return "not implemented"
 }
