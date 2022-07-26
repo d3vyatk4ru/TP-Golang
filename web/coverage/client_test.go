139713d7f8e5e46d0c9a594987937eb68e681a46
d3vyatk4ru <danya.devyatkin@mail.ru>
2022-07-26 14:56:33 +0300

    90% coverage client.go

--- web/coverage/client_test.go
+++ web/coverage/client_test.go
@@ -1,196 +1,356 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 	"time"
 )
 
 type TestCase struct {
 	Request     SearchRequest
+	Response    SearchResponse
 	returnValue error
 	Client      SearchClient
 }
 
 func TestFindUsersBadLimit(t *testing.T) {
 
-	cases := []TestCase{
-		{
-			Client: SearchClient{
-				URL:         "localhost:8080",
-				AccessToken: "AccessToken",
-			},
-			Request: SearchRequest{
-				Limit:      -1,
-				Offset:     1,
-				Query:      "abc",
-				OrderField: "cba",
-				OrderBy:    0,
-			},
-			returnValue: fmt.Errorf("limit must be > 0")},
+	Client := SearchClient{
+		URL:         "localhost:8080",
+		AccessToken: "AccessToken",
+	}
+	Request := SearchRequest{
+		Limit:      -1,
+		Offset:     1,
+		Query:      "abc",
+		OrderField: "Name",
+		OrderBy:    0,
 	}
 
-	for num, item := range cases {
+	returnError := fmt.Errorf("limit must be > 0")
 
-		_, err := item.Client.FindUsers(item.Request)
+	_, err := Client.FindUsers(Request)
 
-		if err.Error() != item.returnValue.Error() {
-			t.Errorf("[TestFindUsersBadLimit](%d) expected error, got %e", num, err)
-		}
+	if err.Error() != returnError.Error() {
+		t.Errorf("[TestFindUsersBadLimit] expected error, got %v", err.Error())
 	}
 }
 
 func TestFindUsersBadOffsetAndDecreaseLimit(t *testing.T) {
 
-	cases := []TestCase{
-		{
-			Client: SearchClient{
-				URL:         "localhost:8080",
-				AccessToken: "AccessToken",
-			},
-			Request: SearchRequest{
-				Limit:      26,
-				Offset:     -1,
-				Query:      "abc",
-				OrderField: "cba",
-				OrderBy:    0,
-			},
-			returnValue: fmt.Errorf("offset must be > 0")},
+	Client := SearchClient{
+		URL:         "localhost:8080",
+		AccessToken: "AccessToken",
 	}
 
-	for num, item := range cases {
+	Request := SearchRequest{
+		Limit:      26,
+		Offset:     -1,
+		Query:      "abc",
+		OrderField: "Name",
+		OrderBy:    0,
+	}
 
-		_, err := item.Client.FindUsers(item.Request)
+	returnError := fmt.Errorf("offset must be > 0")
 
-		if err.Error() != item.returnValue.Error() {
-			t.Errorf("[TestFindUsersBadOffsetAnd...](%d) expected error, got %e", num, err)
-		}
+	_, err := Client.FindUsers(Request)
+
+	if err.Error() != returnError.Error() {
+		t.Errorf("[TestFindUsersBadOffsetAnd...] expected error, got %v", err.Error())
 	}
 }
 
 func TestFindUsersBadDo(t *testing.T) {
 
-	cases := []TestCase{
-		{
-			Client: SearchClient{
-				URL:         "www.mail.ru",
-				AccessToken: "AccessToken",
-			},
-			Request: SearchRequest{
-				Limit:      1,
-				Offset:     0,
-				Query:      "abc",
-				OrderField: "cba",
-				OrderBy:    0,
-			},
-			returnValue: fmt.Errorf("unknown error Get www.mail.ru?limit=2&offset=0&order_by=0&order_field=cba&query=abc: unsupported protocol scheme ")},
+	Client := SearchClient{
+		URL:         "www.mail.ru",
+		AccessToken: "AccessToken",
+	}
+	Request := SearchRequest{
+		Limit:      1,
+		Offset:     0,
+		Query:      "abc",
+		OrderField: "Name",
+		OrderBy:    0,
+	}
+	returnError := fmt.Errorf(`unknown error Get "www.mail.ru?limit=2&offset=0&order_by=0&order_field=Name&query=abc": unsupported protocol scheme ""`)
+
+	_, err := Client.FindUsers(Request)
+
+	if err.Error() != returnError.Error() {
+		t.Errorf("[TestFindUsersBadDo] %v", err.Error())
 	}
 
-	for num, item := range cases {
-
-		_, err := item.Client.FindUsers(item.Request)
-
-		if err.Error() != item.returnValue.Error() {
-			t.Errorf("[TestFindUsersBadDo](%d) expected error, got %e", num, err)
-		}
-	}
 }
 
 func TestFindUsersBadDoTimeout(t *testing.T) {
 
 	// создаем липовый сервер
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(time.Second)
-	}))
+	ts := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(time.Second)
+		}),
+	)
 
-	cases := []TestCase{
-		{
-			Client: SearchClient{
-				URL:         ts.URL,
-				AccessToken: "AccessToken",
-			},
-			Request: SearchRequest{
-				Limit:      1,
-				Offset:     0,
-				Query:      "abc",
-				OrderField: "cba",
-				OrderBy:    0,
-			},
-			returnValue: fmt.Errorf("timeout for limit=2&offset=0&order_by=0&order_field=cba&query=abc")},
+	defer ts.Close()
+
+	Client := SearchClient{
+		URL:         ts.URL,
+		AccessToken: "AccessToken",
 	}
+	Request := SearchRequest{
+		Limit:      1,
+		Offset:     0,
+		Query:      "abc",
+		OrderField: "Name",
+		OrderBy:    0,
+	}
+	returnError := fmt.Errorf("timeout for limit=2&offset=0&order_by=0&order_field=Name&query=abc")
 
-	for num, item := range cases {
+	_, err := Client.FindUsers(Request)
 
-		_, err := item.Client.FindUsers(item.Request)
-
-		if err.Error() != item.returnValue.Error() {
-			t.Errorf("[TestFindUsersBadDoTimeout](%d) expected error, got %e", num, err)
-		}
+	if err.Error() != returnError.Error() {
+		t.Errorf("[TestFindUsersBadDoTimeout] expected error, got %v", err.Error())
 	}
 }
 
 func TestFindUsersStatusUnauthorized(t *testing.T) {
 
-	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+	ts := httptest.NewServer(
+		http.HandlerFunc(SearchServer),
+	)
 
-	cases := []TestCase{
-		{
-			Client: SearchClient{
-				URL:         ts.URL,
-				AccessToken: "BadAccessToken",
-			},
-			Request: SearchRequest{
-				Limit:      1,
-				Offset:     0,
-				Query:      "abc",
-				OrderField: "cba",
-				OrderBy:    0,
-			},
-			returnValue: fmt.Errorf("bad AccessToken")},
+	defer ts.Close()
+
+	Client := SearchClient{
+		URL:         ts.URL,
+		AccessToken: "BadAccessToken",
 	}
+	Request := SearchRequest{
+		Limit:      1,
+		Offset:     0,
+		Query:      "abc",
+		OrderField: "Name",
+		OrderBy:    0,
+	}
+	returnError := fmt.Errorf("bad AccessToken")
 
-	for num, item := range cases {
+	_, err := Client.FindUsers(Request)
 
-		_, err := item.Client.FindUsers(item.Request)
-
-		if err.Error() != item.returnValue.Error() {
-			t.Errorf("[TestFindUsersStatusUnauthorized](%d) expected error, got %e", num, err)
-		}
+	if err.Error() != returnError.Error() {
+		t.Errorf("[TestFindUsersStatusUnauthorized] expected error, got %v", err.Error())
 	}
 }
 
-func TestFindUsers(t *testing.T) {
+func StatusInternalServerError(t *testing.T) {
 
-	ts := httptest.NewServer((http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusInternalServerError)
-	})))
+	ts := httptest.NewServer(
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusInternalServerError)
+			}),
+	)
 
-	cases := []TestCase{
-		{
-			Client: SearchClient{
-				URL:         ts.URL,
-				AccessToken: "AccessToken",
-			},
-			Request: SearchRequest{
-				Limit:      1,
-				Offset:     0,
-				Query:      "abc",
-				OrderField: "cba",
-				OrderBy:    0,
-			},
-			returnValue: fmt.Errorf("SearchServer fatal error")},
+	defer ts.Close()
+
+	Client := SearchClient{
+		URL:         ts.URL,
+		AccessToken: "AccessToken",
+	}
+	Request := SearchRequest{
+		Limit:      1,
+		Offset:     0,
+		Query:      "abc",
+		OrderField: "Name",
+		OrderBy:    0,
+	}
+	returnError := fmt.Errorf("SearchServer fatal error")
+
+	_, err := Client.FindUsers(Request)
+
+	if err.Error() != returnError.Error() {
+		t.Errorf("[TestFindUsers] expected error, got %e", err)
+	}
+}
+
+func TestStatusBadRequestBadUnpackedJson(t *testing.T) {
+
+	ts := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusBadRequest)
+		}),
+	)
+
+	defer ts.Close()
+
+	Client := SearchClient{
+		URL:         ts.URL,
+		AccessToken: "AccessToken",
+	}
+	Request := SearchRequest{
+		Limit:      1,
+		Offset:     0,
+		Query:      "abc",
+		OrderField: "Name",
+		OrderBy:    0,
+	}
+	returnError := fmt.Errorf("cant unpack error json: unexpected end of JSON input")
+
+	_, err := Client.FindUsers(Request)
+
+	if err.Error() != returnError.Error() {
+		t.Errorf("[TestStatusBadRequestBadUnpackedJson] expected error, got %v", err.Error())
+	}
+}
+
+func TestStatusBadRequest(t *testing.T) {
+
+	result, err := json.Marshal(
+		map[string]string{
+			"error": "something went wrong",
+		},
+	)
+
+	ts := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write(result)
+		}),
+	)
+
+	defer ts.Close()
+
+	Client := SearchClient{
+		URL:         ts.URL,
+		AccessToken: "AccessToken",
+	}
+	Request := SearchRequest{
+		Limit:      1,
+		Offset:     0,
+		Query:      "abc",
+		OrderField: "Name",
+		OrderBy:    0,
 	}
 
-	httptest.NewServer(http.HandlerFunc(SearchServer))
+	returnError := fmt.Errorf("unknown bad request error: something went wrong")
 
-	for _, item := range cases {
+	_, err = Client.FindUsers(Request)
 
-		_, err := item.Client.FindUsers(item.Request)
+	if err.Error() != returnError.Error() {
+		t.Errorf("[TestStatusBadRequestBadUnpackedJson] expected error, got %v", err.Error())
+	}
+}
+func TestStatusBadRequestErrorBadOrderField(t *testing.T) {
 
-		if err.Error() != item.returnValue.Error() {
+	result, _ := json.Marshal(
+		SearchErrorResponse{Error: `OrderField invalid`},
+	)
 
-		}
+	ts := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write(result)
+		}),
+	)
+
+	defer ts.Close()
+
+	Client := SearchClient{
+		AccessToken: "AccessToken",
+		URL:         ts.URL,
+	}
+	Request := SearchRequest{
+		Limit:      1,
+		Offset:     0,
+		Query:      "abc",
+		OrderField: "InvalidOrderField",
+		OrderBy:    1,
+	}
+	returnError := fmt.Errorf("OrderField InvalidOrderField invalid")
+
+	_, err := Client.FindUsers(Request)
+
+	if err.Error() != returnError.Error() {
+		t.Errorf("[TestStatusBadRequestErrorBadOrderField] expected error, got %v", err.Error())
+	}
+}
+
+// func TestBadUnpackedJson(t *testing.T) {
+
+// 	result, _ := json.Marshal([]User{})
+
+// 	ts := httptest.NewServer(
+// 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 			w.Write(result)
+// 		}),
+// 	)
+
+// 	defer ts.Close()
+
+// 	Client := SearchClient{
+// 		URL:         ts.URL,
+// 		AccessToken: "AccessToken",
+// 	}
+// 	Request := SearchRequest{
+// 		Limit:      1,
+// 		Offset:     0,
+// 		Query:      "abc",
+// 		OrderField: "Name",
+// 		OrderBy:    0,
+// 	}
+
+// 	returnValue := fmt.Errorf("cant unpack result json: unexpected end of JSON input")
+
+// 	_, err := Client.FindUsers(Request)
+
+// 	if err.Error() != returnValue.Error() {
+// 		t.Errorf("[TestBadUnpackedJson] expected error, got %v", err.Error())
+// 	}
+// }
+
+func TestFindUser(t *testing.T) {
+
+	ts := httptest.NewServer(
+		http.HandlerFunc(SearchServer),
+	)
+
+	defer ts.Close()
+
+	Client := SearchClient{
+		URL:         ts.URL,
+		AccessToken: "AccessToken",
+	}
+	Request := SearchRequest{
+		Limit:      1,
+		Offset:     0,
+		Query:      "abc",
+		OrderField: "Name",
+		OrderBy:    0,
+	}
+	Response := SearchResponse{
+		Users: []User{
+			{
+				ID:     999,
+				Name:   "Default",
+				Age:    0,
+				About:  "",
+				Gender: "male",
+			},
+		},
+		NextPage: false,
+	}
+
+	result, err := Client.FindUsers(Request)
+
+	if !reflect.DeepEqual(result.Users, Response.Users) {
+		t.Errorf("[TestBadUnpackedJson] Response is wrong %v", result)
+	}
+
+	if err != nil {
+		t.Errorf("[TestBadUnpackedJson] expected error, got %v", err.Error())
 	}
 }
